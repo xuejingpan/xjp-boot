96ced37be1818100eec52492011c2e4467f70091
xuejingpan <xuejingpan@qq.com>
2023-02-27 23:47:55 +0800

    初始化项目

---
+++ src/main/java/com/xuejingpan/xjpboot/XjpBootApplication.java
@@ -0,0 +1,13 @@
+package com.xuejingpan.xjpboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XjpBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XjpBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuejingpan/xjpboot/XjpBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuejingpan.xjpboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XjpBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
