9113817eb912a386cad173c2190972807a86c36a
xuejingpan <xuejingpan@qq.com>
2023-03-13 23:56:25 +0800

    token常量

---
+++ src/main/java/com/xuejingpan/xjpboot/common/constant/TokenConstant.java
@@ -0,0 +1,26 @@
+package com.xuejingpan.xjpboot.common.constant;
+
+/**
+ * @InterfaceName TokenConstant
+ * @Description TODO
+ * @Author xuejingpan
+ * @Date 2023/3/13 23:23
+ * @Version 1.0
+ */
+public interface TokenConstant {
+
+    /**
+     * 名称
+     */
+    String NAME = "Authorization";
+
+    /**
+     * 前缀
+     */
+    String PREFIX = "Bearer ";
+
+    /**
+     * 秘钥
+     */
+    String SECRET = "freedom";
+}
