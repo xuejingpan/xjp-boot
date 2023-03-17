6b5b20e9be95ad0316468d578bd3f0a57926c41a
xuejingpan <xuejingpan@qq.com>
2023-03-17 23:45:55 +0800

    update README.md

---
+++ src/main/java/com/xuejingpan/xjpboot/common/config/AsyncConfig.java
@@ -0,0 +1,11 @@
+package com.xuejingpan.xjpboot.common.config;
+
+/**
+ * @ClassName AsyncConfig
+ * @Description TODO
+ * @Author xuejingpan
+ * @Date 2023/3/17 23:42
+ * @Version 1.0
+ */
+public class AsyncConfig {
+}
