644608fccf98903c32334471c7436835585af5a1
xuejingpan <xuejingpan@qq.com>
2023-03-17 23:46:33 +0800

    添加线程池

--- src/main/java/com/xuejingpan/xjpboot/common/config/AsyncConfig.java
+++ src/main/java/com/xuejingpan/xjpboot/common/config/AsyncConfig.java
@@ -1,5 +1,14 @@
 package com.xuejingpan.xjpboot.common.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * @ClassName AsyncConfig
  * @Description TODO
@@ -7,5 +16,30 @@ package com.xuejingpan.xjpboot.common.config;
  * @Date 2023/3/17 23:42
  * @Version 1.0
  */
-public class AsyncConfig {
+@EnableAsync
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Bean
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        // 核心线程数
+        executor.setCorePoolSize(10);
+        // 最大线程数
+        executor.setMaxPoolSize(20);
+        // 缓冲队列
+        executor.setQueueCapacity(10);
+        // 空闲时间
+        executor.setKeepAliveSeconds(30);
+        // 前缀名称
+        executor.setThreadNamePrefix("xjp-boot-");
+        // 停机策略
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        // 拒绝策略
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 初始化
+        executor.initialize();
+        return executor;
+    }
 }
