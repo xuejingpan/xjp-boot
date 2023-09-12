7d3c51771a4cf65d745223b71cd7ab6c78b002d0
xuejingpan <xuejingpan@qq.com>
2023-09-13 00:57:52 +0800

    .

--- src/main/java/com/xuejingpan/xjpboot/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/xuejingpan/xjpboot/common/handler/GlobalExceptionHandler.java
@@ -2,23 +2,22 @@ package com.xuejingpan.xjpboot.common.handler;
 
 import com.xuejingpan.xjpboot.common.exception.AuthenticationException;
 import com.xuejingpan.xjpboot.common.exception.AuthorizationException;
-import com.xuejingpan.xjpboot.common.exception.ExceptionData;
 import com.xuejingpan.xjpboot.common.exception.GlobalException;
 import com.xuejingpan.xjpboot.common.result.ResponseResult;
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import javax.validation.ValidationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @ClassName GlobalExceptionHandler
@@ -57,48 +56,50 @@ public class GlobalExceptionHandler {
         return ResponseResult.fail(e.getMessage());
     }
 
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler({BindException.class,
-//            ConstraintViolationException.class,
-//            MissingServletRequestParameterException.class})
-//    public Result<?> handleParamVerificationException(Exception e) {
-//        StringBuilder errorMessage = new StringBuilder();
-//        if (e instanceof BindException) {
-//            BindingResult result = ((BindException) e).getBindingResult();
-//            result.getAllErrors().forEach(error -> errorMessage.append(error.getDefaultMessage()).append("、"));
-//        } else if (e instanceof ConstraintViolationException) {
-//            Set<ConstraintViolation<?>> result = ((ConstraintViolationException) e).getConstraintViolations();
-//            result.forEach(error -> errorMessage.append("[").append(error.getMessage()).append("]"));
-//        } else if (e instanceof MissingServletRequestParameterException) {
-//            MissingServletRequestParameterException exception = (MissingServletRequestParameterException) e;
-//            errorMessage.append(exception.getParameterName()).append("不能为空");
-//        }
-//        log.warn("参数校验异常: {}", e.toString());
-//        return Result.error(errorMessage.toString());
-//    }
-
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(value = { BindException.class, ValidationException.class, MethodArgumentNotValidException.class })
-    public ResponseResult<?> handleParameterVerificationException(@NonNull Exception e) {
-        ExceptionData.ExceptionDataBuilder exceptionDataBuilder = ExceptionData.builder();
-        if (e instanceof BindException) {
-            BindingResult bindingResult = ((MethodArgumentNotValidException) e).getBindingResult();
-            bindingResult.getAllErrors()
-                    .forEach(a -> exceptionDataBuilder.data(((FieldError) a).getField() + ": " + a.getDefaultMessage()));
-        } else if (e instanceof ConstraintViolationException) {
-            if (e.getMessage() != null) {
-                ResponseResult.fail(e.getMessage());
-            }
-        } else {
-            ResponseResult.fail("invalid parameter");
-        }
-        return ResponseResult.fail("invalid parameter");
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseResult<?> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        log.warn("参数校验异常: {}", e.toString());
+        return ResponseResult.fail(e.getParameterName() + "不能为空");
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({BindException.class, ConstraintViolationException.class})
+    public ResponseResult<?> handleParamVerification(Exception e) {
+        List<String> errorMessage = new ArrayList<>();
+        if (e instanceof BindException) {
+            BindingResult result = ((BindException) e).getBindingResult();
+            result.getAllErrors().forEach(error -> errorMessage.add(error.getDefaultMessage()));
+        } else if (e instanceof ConstraintViolationException) {
+            Set<ConstraintViolation<?>> result = ((ConstraintViolationException) e).getConstraintViolations();
+            result.forEach(error -> errorMessage.add(error.getMessage()));
+        }
+        log.warn("参数校验异常: {}", e.toString());
+        return ResponseResult.fail(errorMessage.toString());
+    }
+
+//    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+//    @ExceptionHandler(value = { BindException.class, ValidationException.class, MethodArgumentNotValidException.class })
+//    public ResponseResult<?> handleParameterVerificationException(@NonNull Exception e) {
+//        ExceptionData.ExceptionDataBuilder exceptionDataBuilder = ExceptionData.builder();
+//        if (e instanceof BindException) {
+//            BindingResult bindingResult = ((MethodArgumentNotValidException) e).getBindingResult();
+//            bindingResult.getAllErrors()
+//                    .forEach(a -> exceptionDataBuilder.data(((FieldError) a).getField() + ": " + a.getDefaultMessage()));
+//        } else if (e instanceof ConstraintViolationException) {
+//            if (e.getMessage() != null) {
+//                ResponseResult.fail(e.getMessage());
+//            }
+//        } else {
+//            ResponseResult.fail("invalid parameter");
+//        }
+//        return ResponseResult.fail("invalid parameter");
+//    }
     
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Throwable.class)
-    public ResponseResult<?> handleThrowable(Throwable e) {
-        log.error("{}: {}", e.toString(), e.getStackTrace());
-        return ResponseResult.fail();
+    public ResponseResult<?> handleThrowable(Throwable throwable) {
+        log.error("内部错误: {}", throwable.toString());
+        return ResponseResult.fail(throwable.getMessage());
     }
 }
